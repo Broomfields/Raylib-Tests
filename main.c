08b1bd625c0e473e1eb78be877d7103e434e41bc
Shaun Broomfield <36537416+Enstroe@users.noreply.github.com>
2021-11-09 07:46:00 +0000

    small animation test

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib");
+    InitWindow(screenWidth, screenHeight, "Ya Boi Rendering like a digital Bob Ross");
 
     Camera camera = { 0 };
     camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
@@ -18,14 +18,33 @@ int main()
     
     SetCameraMode(camera, CAMERA_ORBITAL);
 
-    Vector3 cubePosition = { 0 };
+    // Vector3 cubePosition = { 0 };
+    float startX = 100;
+    float startY = 100;
+    float endX = 500;
+    float endY = 250;
 
-    SetTargetFPS(120);               // Set our game to run at 60 frames-per-second
+    float adj = 2;
+
+    float thick = 12;
+    float fontSize = 20;
+    Color colour = RAYWHITE;
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if (startX > 500)
+        {
+            adj = -1;
+        }
+        else if (startX < 100)
+        {
+            adj = 1;
+        }
+
         // Update
         //----------------------------------------------------------------------------------
         UpdateCamera(&camera);
@@ -35,22 +54,33 @@ int main()
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(DARKGRAY);
+            
+            // BeginMode3D(camera);
+            // DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+            // DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+            // DrawGrid(10, 1.0f);
+            // EndMode3D();
 
-            BeginMode3D(camera);
+            DrawLineBezier((Vector2){startX, startY}, (Vector2){endX, endY}, thick, RED);
+            DrawLineBezierQuad((Vector2){startX, startY}, (Vector2){endX, endY}, (Vector2){endX - startX, endY - startY}, thick, GREEN);
+            DrawLineBezierCubic((Vector2){startX, startY}, (Vector2){endX, endY}, (Vector2){endX - startX, endY - startY}, (Vector2){endY - startY, endX - startX}, thick, BLUE);
+           
+            // DrawLineBezier((Vector2){startX, endY}, (Vector2){endX, startY}, thick, colour);
+            // DrawLineBezierQuad((Vector2){startX, endY}, (Vector2){endX, startY}, (Vector2){endX - startX, endY - startY}, thick, colour);
+            // DrawLineBezierCubic((Vector2){startX, endY}, (Vector2){endX, startY}, (Vector2){endX - startX, endY - startY}, (Vector2){endY - startY, endX - startX}, thick, colour);
 
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-                DrawGrid(10, 1.0f);
-
-            EndMode3D();
-
-            DrawText("HOLY FUCKING SHIT BRO!", 10, 40, 20, DARKGRAY);
+            DrawText("Origin", startX, startY - (fontSize * 1.5), fontSize, colour);
 
             DrawFPS(10, 10);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
+
+        startX += adj;
+        startY += (adj / 2);
+
+    
     }
 
     // De-Initialization
